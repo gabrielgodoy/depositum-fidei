b61c59b1fd342d70623b21228bf383efe975a90d
Gabriel Godoy <gabrielgodoyon@gmail.com>
2022-02-19 21:34:51 -0300

    fix: common commit

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
 };
 
 module.exports = nextConfig;
