4042e93272910b4045d8e685b91beb2b0cc22d65
Gabriel Godoy <gabrielgodoyon@gmail.com>
2022-02-19 22:58:00 -0300

    fix: common commit

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,19 +1,27 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
+import { pages } from "pages/constants";
+
+import style from "./SideMenu.module.scss";
 
 export const SideMenu = () => {
   const { t } = useTranslation();
+  const router = useRouter();
 
   return (
-    <div
-      style={{
-        background: "#e5e5e5",
-        padding: "20px",
-        marginBottom: "20px",
-      }}
-    >
-      <Link href="/church-fathers" passHref>
-        <button type="button">{t("toSecondPage")}</button>
+    <div className={style.sideMenu}>
+      <div className={style.links}>
+        <Link href={pages.home.path} passHref>
+          <button type="button">Home</button>
+        </Link>
+        <Link href={pages.churchFathers.path} passHref>
+          <button type="button">{pages.churchFathers.title}</button>
+        </Link>
+      </div>
+
+      <Link href="/" locale={router.locale === "en" ? "pt-BR" : "en"} passHref>
+        <button>{t("changeLocale")}</button>
       </Link>
     </div>
   );
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,7 @@
 import johnPaulii from "assets/images/john-paul-ii.jpeg";
 import { Layout } from "components";
-import { SideMenu } from "components/SideMenu";
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
@@ -14,19 +10,10 @@ import styles from "./index.module.scss";
 
 const Home: NextPage = () => {
   const { t } = useTranslation();
-  const router = useRouter();
 
   return (
     <Layout pageDetail={pages.home}>
       <div className={styles.container}>
-        <Link
-          href="/"
-          locale={router.locale === "en" ? "pt-BR" : "en"}
-          passHref
-        >
-          <button>{t("changeLocale")}</button>
-        </Link>
-
         <h1>{t("whatDepositumFideiMeans", { ns: "home" })}</h1>
 
         <p>
